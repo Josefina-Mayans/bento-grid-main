412f7f5b770c8c8497b67c3e9773f08963171850
Josefina-Mayans <jmayans98@gmail.com>
2025-04-30 12:08:12 -0300

    first commit with files

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      Social Media 10x Faster with AI
+    Over 4,000 5-star reviews
+  
+    Manage multiple accounts and platforms.
+  
+    Maintain a consistent posting schedule.
+  
+    Schedule to social media.
+    Optimize post timings to publish content at the perfect time for your audience.
+    
+    Grow followers with non-stop content.
+  
+    -56% faster audience growth
+    
+    Create and schedule content quicker.
+  
+    Write your content using AI.
+    <div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+    </>
+  )
+}
+
+export default App
